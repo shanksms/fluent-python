bb79bd1feb25d2032b691b177c67cde6d16f37e2
unknown <sriv.srivastava@gmail.com>
2021-03-03 15:18:50 +0530

    dictionary and sets. first commit

---
+++ index.py
@@ -0,0 +1,19 @@
+import sys
+import re
+
+WORD_RE = re.compile(r'\w+')
+index = {}
+
+with open('zen.txt', encoding='utf-8') as fp:
+    for line_no, line in enumerate(fp, 1):
+        for match in WORD_RE.finditer(line):
+            word = match.group();
+            column_no = match.start() + 1
+            location = (line_no, column_no)
+            #below three lines can be replaced with one line. see index.py
+            index.setdefault(word, []).append(location)
+
+
+for word in sorted(index, key=str.upper):
+    print(word, index[word])
+
